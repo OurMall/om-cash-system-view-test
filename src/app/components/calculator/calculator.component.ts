47f70081f4d3625ba21e8bd65a8454b4e3b561fe
Naranjo17 <juanjosenaranjobeltran@gmail.com>
2022-06-14 00:15:04 -0500

    Create service convert price dollar

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ICreateOrderRequest, IPayPalConfig } from 'ngx-paypal';
 import { enviroment } from 'src/app/models/enviroment';
+import { Invoice } from 'src/app/models/invoice';
+import { InvoiceService } from 'src/app/services/ServicesInvoices/invoice.service';
+import { ServicesPriceDollarServiceService } from 'src/app/services/ServicesPriceDollar/ServicesPriceDollarService.service';
 
 @Component({
   selector: 'app-calculator',
@@ -12,28 +15,32 @@ export class CalculatorComponent implements OnInit {
   public payPalConfig ? : IPayPalConfig;
   public total_price:number;
   public products: any[] = [];
-  constructor() { }
+  public priceDollar!: number;
+  public dollarInPeso!: number;
+  showCancel!: boolean;
+  showSuccess!: boolean;
+  showError!: boolean;
+  invoices: Invoice;
 
-  ngOnInit(): void {
+  constructor(
+    private apiInvoice: InvoiceService,
+    private apiDollar: ServicesPriceDollarServiceService
+  ) {
     this.getInvoiceStorage();
-    this.initConfig();
-  }
-
-  getInvoiceStorage() {
-    let total_price = localStorage.getItem('total_price');
-    this.total_price = Number(total_price);
-
-    let products = localStorage.getItem('product');
-    if (products == null) {
-      this.products = [];
-    } else {
-      this.products = JSON.parse(products);
-      console.log(this.products,"products");
-      
-    };
   };
 
-  private initConfig(): void {
+  ngOnInit(): void {
+    this.getPriceDollar()
+    console.log(this.dollarInPeso,'precio en pesos')
+
+  };
+
+  receiveInovice($event:Invoice) {
+    this.invoices = $event;
+  };
+
+
+  private initConfig(price:number,valuePeso:number): void {
     this.payPalConfig = {
         currency: 'USD',
         clientId: enviroment.clientId,
@@ -42,15 +49,15 @@ export class CalculatorComponent implements OnInit {
             purchase_units: [{
                 amount: {
                     currency_code: 'USD',
-                    value: this.total_price.toString(),
+                    value: price.toString(),
                     breakdown: {
                         item_total: {
                             currency_code: 'USD',
-                            value: this.total_price.toString()
+                            value: price.toString()
                         }
                     }
                 },
-                items: this.getItemList()
+                items: this.getItemList(valuePeso)
             }]
         },
         advanced: {
@@ -68,7 +75,7 @@ export class CalculatorComponent implements OnInit {
 
         },
         onClientAuthorization: (data) => {
-            console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', 
+            console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point',
             JSON.stringify(data));
             //this.showSuccess = true;
         },
@@ -88,15 +95,16 @@ export class CalculatorComponent implements OnInit {
     };
 };
 
-  getItemList(): any[] {
+  getItemList(pricePesos:number): any[] {
     const items: any[] = [];
+    console.log(pricePesos)
     let item = {};
     this.products.forEach((element: any) => {
       item = {
         name: element.Name,
         quantity: element.Quantity,
         unit_amount: {
-          value: element.Price,
+          value:  Math.round((element.Price * element.Quantity )/ pricePesos),
           currency_code:'USD'
         }
       };
@@ -105,4 +113,34 @@ export class CalculatorComponent implements OnInit {
     return items;
   };
 
-}
+
+  getInvoiceStorage() {
+    let total_price = localStorage.getItem('total_price');
+    this.total_price = Number(total_price);
+    let products = localStorage.getItem('product');
+    if (products == null) {
+      this.products = [];
+    } else {
+      this.products = JSON.parse(products);
+    };
+  };
+
+  getPriceDollar() {
+    let priceDollarInPeso:number = 0;
+    this.apiDollar.getDates().subscribe((dollar:any) => {
+      priceDollarInPeso = dollar.rates.COP
+      this.calculatePriceDollar(priceDollarInPeso);
+    });
+  };
+
+  calculatePriceDollar(priceDollar:number):number {
+    let range:number = this.getItemList(priceDollar).length;
+    let price:number = 0;
+    for (let index = 0; index < range; index++) {
+      price += (this.getItemList(priceDollar)[index].unit_amount.value);
+    };
+    this.initConfig(price,priceDollar);
+    return price;
+  };
+
+  }
